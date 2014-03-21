84e694038e734b7e4210c9cf82f8c286903720d4
Nicolai Steffensen <nicolai.steffensen@gmail.com>
2014-03-21 09:45:50 +0100

    Misc changes
    Returning dictionary with values now. Added some more information

--- Eurisco.py
+++ Eurisco.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 from doctest import debug
 from suds.client import Client
 import datetime
+import pytz
 
 __author__ = 'Nicolai'
 
@@ -35,25 +36,37 @@ class EuriscoData:
     # Request GetFlexPrice from the web service
     return client.service.GetFlexPrice(requestId, type, date, duration)
 
-  def getTestData(self, type, duration):
-    return self.__fetch_data(type, duration)
-
+  """
+  This methoid will return a dictionary with price, currency,
+  date, units.
+  @return dictionary with price, currency, data, units
+  @rtype dict
+  """
   def getCurrentPrice(self):
 
+    flexInfo = {}
     price = 0
     result = self.__fetch_data(1, 7200)
     for flexPriceSignal in result:
+      #print(flexPriceSignal)
       for x in flexPriceSignal:
         if x[0] == 'entries':
           for entry in x:
             if isinstance(entry, str):
               continue
-            price = entry[0]['value']
-            break
-      return price
+            flexInfo['price'] = entry[0]['value']
+            flexInfo['datetime'] = (entry[0]['startingTime'] - datetime.datetime(1970,1,1,tzinfo=pytz.utc)).total_seconds()
+        elif x[0] == 'signalType':
+          for entry in x:
+            if isinstance(entry, str):
+              continue
+            flexInfo['unit'] = entry['perUnit']
+            flexInfo['currency'] = entry['currency']
+
+      return flexInfo
 
 
 if __name__ == '__main__':
 
-  e = EuriscoData("1013","1013")
+  e = EuriscoData("1013", "1013")
   print(e.getCurrentPrice())
