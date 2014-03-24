7735300f3b38b25616a2a341ed532aec501c8e6a
Bo Fjord Jensen <bo@bxd.dk>
2014-03-24 15:08:01 +0100

    Start making this into a simple package for getting info from Eurisco and move everything else into a seperate package

---
+++ eurisco/constants.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+__author__ = 'Nicolai Steffensen, Rene Jacobsen, and Bo Fjord Jensen'
+__copyright__ = "Copyright 2014, Vi tre her"
+__license__ = "All rights reserved"
+__version__ = "0.0.1"
+__status__ = "Prototype"
+
+default_eurisco_url = "http://backend.eurisco.dk:4040/webservices/flexprice?wsdl"
